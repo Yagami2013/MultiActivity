3a10788258e5ead18635c0ee9fd6fe4f12245fa6
Yagami2013 <489677274@qq.com>
2013-12-20 00:11:37 +0800

    finish multi-activity with error

--- MultiActivity/src/com/example/multiactivity/MainActivity.java
+++ MultiActivity/src/com/example/multiactivity/MainActivity.java
@@ -2,14 +2,27 @@ package com.example.multiactivity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		Button nextButton = (Button)findViewById(R.id.next);
+		nextButton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this,OtherActivity.class);
+				
+			}
+		});
+		
 	}
 
 	
--- MultiActivity/src/com/example/multiactivity/OtherActivity.java
+++ MultiActivity/src/com/example/multiactivity/OtherActivity.java
@@ -12,11 +12,4 @@ public class OtherActivity extends Activity {
 		setContentView(R.layout.activity_other);
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.other, menu);
-		return true;
-	}
-
 }
