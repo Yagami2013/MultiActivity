097c64a3b832ec72ce8a6c658bb8ceb63c8d598f
Yagami <489677274@qq.com>
2013-12-23 18:11:41 +0800

    finish multi-activity coding

--- MultiActivity/src/com/example/multiactivity/OtherActivity.java
+++ MultiActivity/src/com/example/multiactivity/OtherActivity.java
@@ -2,7 +2,11 @@ package com.example.multiactivity;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class OtherActivity extends Activity {
 
@@ -10,6 +14,31 @@ public class OtherActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_other);
+		Intent intent = this.getIntent();
+		
+		Bundle bundle= intent.getExtras();
+		String name=bundle.getString("name");
+		int age=bundle.getInt("age");
+		TextView paramView = (TextView) this.findViewById(R.id.Other);
+        paramView.setText("名称："+ name + "  年龄："+ age);
+        
+        Button button=(Button)findViewById(R.id.close);
+        button.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View V) {
+				Intent data=new Intent();
+				data.putExtra("name", "王二");
+				data.putExtra("age", 23);
+				
+				OtherActivity.this.setResult(1,data);
+				OtherActivity.this.finish();
+				
+				
+			}
+		});
+
+
 	}
 
 }
